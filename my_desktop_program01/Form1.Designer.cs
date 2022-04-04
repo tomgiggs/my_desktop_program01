46a2ae525a4e81eb230c9acea8c9a868434c017b
tomgigg <zamjbn@163.com>
2022-04-05 00:11:03 +0800

    feat: 新增安装包配置

--- my_desktop_program01/Form1.Designer.cs
+++ my_desktop_program01/Form1.Designer.cs
@@ -28,13 +28,298 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.信息ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.另存为ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.最近ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.编辑ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.撤销ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.重做ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.段落ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.表格ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.图片ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.查找ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.id_box = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.addr_box = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.name_box = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.age_box = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.submit = new System.Windows.Forms.Button();
+            this.sex_box = new System.Windows.Forms.ListBox();
+            this.result_text = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.select_upload_file = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem,
+            this.编辑ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.信息ToolStripMenuItem,
+            this.另存为ToolStripMenuItem,
+            this.最近ToolStripMenuItem});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // 信息ToolStripMenuItem
+            // 
+            this.信息ToolStripMenuItem.Name = "信息ToolStripMenuItem";
+            this.信息ToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.信息ToolStripMenuItem.Text = "信息";
+            // 
+            // 另存为ToolStripMenuItem
+            // 
+            this.另存为ToolStripMenuItem.Name = "另存为ToolStripMenuItem";
+            this.另存为ToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.另存为ToolStripMenuItem.Text = "另存为";
+            // 
+            // 最近ToolStripMenuItem
+            // 
+            this.最近ToolStripMenuItem.Name = "最近ToolStripMenuItem";
+            this.最近ToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.最近ToolStripMenuItem.Text = "最近";
+            // 
+            // 编辑ToolStripMenuItem
+            // 
+            this.编辑ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.撤销ToolStripMenuItem,
+            this.重做ToolStripMenuItem,
+            this.段落ToolStripMenuItem,
+            this.表格ToolStripMenuItem,
+            this.图片ToolStripMenuItem,
+            this.查找ToolStripMenuItem});
+            this.编辑ToolStripMenuItem.Name = "编辑ToolStripMenuItem";
+            this.编辑ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.编辑ToolStripMenuItem.Text = "编辑";
+            // 
+            // 撤销ToolStripMenuItem
+            // 
+            this.撤销ToolStripMenuItem.Name = "撤销ToolStripMenuItem";
+            this.撤销ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.撤销ToolStripMenuItem.Text = "撤销";
+            // 
+            // 重做ToolStripMenuItem
+            // 
+            this.重做ToolStripMenuItem.Name = "重做ToolStripMenuItem";
+            this.重做ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.重做ToolStripMenuItem.Text = "重做";
+            // 
+            // 段落ToolStripMenuItem
+            // 
+            this.段落ToolStripMenuItem.Name = "段落ToolStripMenuItem";
+            this.段落ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.段落ToolStripMenuItem.Text = "段落";
+            // 
+            // 表格ToolStripMenuItem
+            // 
+            this.表格ToolStripMenuItem.Name = "表格ToolStripMenuItem";
+            this.表格ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.表格ToolStripMenuItem.Text = "表格";
+            // 
+            // 图片ToolStripMenuItem
+            // 
+            this.图片ToolStripMenuItem.Name = "图片ToolStripMenuItem";
+            this.图片ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.图片ToolStripMenuItem.Text = "图片";
+            // 
+            // 查找ToolStripMenuItem
+            // 
+            this.查找ToolStripMenuItem.Name = "查找ToolStripMenuItem";
+            this.查找ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.查找ToolStripMenuItem.Text = "查找";
+            this.查找ToolStripMenuItem.Click += new System.EventHandler(this.查找ToolStripMenuItem_Click);
+            // 
+            // id_box
+            // 
+            this.id_box.Location = new System.Drawing.Point(95, 72);
+            this.id_box.Name = "id_box";
+            this.id_box.Size = new System.Drawing.Size(100, 21);
+            this.id_box.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "身份证号";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(15, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "家庭住址";
+            // 
+            // addr_box
+            // 
+            this.addr_box.Location = new System.Drawing.Point(95, 99);
+            this.addr_box.Name = "addr_box";
+            this.addr_box.Size = new System.Drawing.Size(100, 21);
+            this.addr_box.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(49, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "姓名";
+            // 
+            // name_box
+            // 
+            this.name_box.Location = new System.Drawing.Point(95, 45);
+            this.name_box.Name = "name_box";
+            this.name_box.Size = new System.Drawing.Size(100, 21);
+            this.name_box.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(49, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "年龄";
+            // 
+            // age_box
+            // 
+            this.age_box.Location = new System.Drawing.Point(95, 126);
+            this.age_box.Name = "age_box";
+            this.age_box.Size = new System.Drawing.Size(100, 21);
+            this.age_box.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(49, 152);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "性别";
+            // 
+            // submit
+            // 
+            this.submit.Location = new System.Drawing.Point(65, 211);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(129, 38);
+            this.submit.TabIndex = 11;
+            this.submit.Text = "提交";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // sex_box
+            // 
+            this.sex_box.FormattingEnabled = true;
+            this.sex_box.ItemHeight = 12;
+            this.sex_box.Items.AddRange(new object[] {
+            "男",
+            "女"});
+            this.sex_box.Location = new System.Drawing.Point(96, 156);
+            this.sex_box.Name = "sex_box";
+            this.sex_box.Size = new System.Drawing.Size(98, 28);
+            this.sex_box.TabIndex = 12;
+            this.sex_box.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // result_text
+            // 
+            this.result_text.Location = new System.Drawing.Point(412, 108);
+            this.result_text.Name = "result_text";
+            this.result_text.Size = new System.Drawing.Size(356, 21);
+            this.result_text.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(119, 284);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 29);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "上传文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // select_upload_file
+            // 
+            this.select_upload_file.FileName = "选择上传文件";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.result_text);
+            this.Controls.Add(this.sex_box);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.age_box);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.name_box);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.addr_box);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.id_box);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "个人信息录入";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 信息ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 另存为ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 最近ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 编辑ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 撤销ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 重做ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 段落ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 表格ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 图片ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 查找ToolStripMenuItem;
+        private System.Windows.Forms.TextBox id_box;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox addr_box;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox name_box;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox age_box;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.ListBox sex_box;
+        private System.Windows.Forms.TextBox result_text;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog select_upload_file;
     }
 }
 
--- my_desktop_program01/Form1.cs
+++ my_desktop_program01/Form1.cs
@@ -7,6 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Web;
+using System.Threading;
+using Newtonsoft.Json;
+using Flurl;
+using Flurl.Http;
 
 namespace my_desktop_program01
 {
@@ -16,5 +21,72 @@ namespace my_desktop_program01
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 查找ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void submit_Click(object sender, EventArgs e)
+        {
+            PrivateUserInfo user_info = new PrivateUserInfo();
+            user_info.UserName = name_box.Text;
+            user_info.UserId = id_box.Text;
+            user_info.UserAddr = addr_box.Text;
+            user_info.UserAge = int.Parse(age_box.Text);
+            user_info.UserSex = sex_box.Text;
+            string json_body = Newtonsoft.Json.JsonConvert.SerializeObject(user_info);
+            string text = await "https://baidu.com".GetStringAsync();
+            result_text.Text = text;
+
+
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = select_upload_file.ShowDialog();
+            //result.ToString();
+            result_text.Text = select_upload_file.FileName;
+            //select_upload_file.
+        }
+    }
+    public class PrivateUserInfo
+    {
+        public string UserName;
+        public string UserId;
+        public string UserAddr;
+        public Int32 UserAge;
+        public string UserSex;
     }
 }
